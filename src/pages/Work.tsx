7fbe32d2245711f5f9d56334ae446c7cbbcc0c9b
Destiny Ayomah <ayomahdestiny@gmail.com>
2023-09-26 20:22:40 +0100

    fixed menu on work page

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -21,174 +21,177 @@ const Work = ({ isMenuOpen, setIsMenuOpen }: Props) => {
     }
 
     return <>
-        <Show below="md">
-            <HStack padding='15px 5%'>
-                <MobileMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-            </HStack>
-        </Show>
+        <Box padding='15px 5%'>
+            <Show below="md">
+                <HStack>
+                    <MobileMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+                </HStack>
+            </Show>
 
-        <VStack flex='1' color={textPrimary} alignItems='start' overflow='auto' css={hiddenScrollbar}>
-            <Text fontWeight='700' >FEATURED</Text>
+            <VStack flex='1' marginTop='30px' color={textPrimary} alignItems='start' overflow='auto' css={hiddenScrollbar}>
+                <Text fontWeight='700' fontSize={{base: '12px', md: '16px'}}>FEATURED</Text>
 
-            <VStack marginTop='20px' gap='20px' width='100%'>
-                <VStack spacing='20px' bg={bgPrimary} padding='30px 30px' borderRadius='10px' width='100%'>
-                    <Box width='100%'>
-                        <Text color='white' width='100%' fontWeight='600' fontSize='30px'>Lensture</Text>
-                        <Text color='#BEBEBE' width='100%'>REACTJS + TYPESCRIPT + CHAKRA-UI</Text>
-                    </Box>
-                    <Text width='100%'>A dynamic photo library that allows authenticated users to perform sorting of the gallary.</Text>
+                <VStack marginTop='20px' gap='20px' width='100%'>
+                    <VStack spacing='20px' bg={bgPrimary} padding='30px 30px' borderRadius='10px' width='100%'>
+                        <Box width='100%'>
+                            <Text color='white' width='100%' fontWeight='600' fontSize='30px'>Lensture</Text>
+                            <Text color='#BEBEBE' width='100%'>REACTJS + TYPESCRIPT + CHAKRA-UI</Text>
+                        </Box>
+                        
+                        <Text width='100%'>A dynamic photo library that allows authenticated users to perform sorting of the gallary.</Text>
 
-                    <HStack width='100%'>
-                        <FontAwesomeIcon color="#BEBEBE" icon={faEye} />
-                        <Text color='#BEBEBE'>Live</Text>
-                    </HStack>
-                </VStack>
-
-                <VStack spacing='20px' bg={bgPrimary} padding='30px 30px' borderRadius='10px' width='100%'>
-                    <Box width='100%'>
-                        <Text color='white' width='100%' fontWeight='600' fontSize='30px'>GAME HUB</Text>
-                        <Text color='#BEBEBE' width='100%'>REACTJS + TYPESCRIPT + CHAKRA-UI</Text>
-                    </Box>
-                    <Text width='100%'>A video game discovery platform for game lovers.</Text>
-
-                    <HStack width='100%'>
-                        <HStack>
+                        <HStack width='100%'>
                             <FontAwesomeIcon color="#BEBEBE" icon={faEye} />
                             <Text color='#BEBEBE'>Live</Text>
                         </HStack>
+                    </VStack>
 
-                        <HStack>
-                            <FontAwesomeIcon color="#BEBEBE" icon={faCodeBranch} />
-                            <Text color='#BEBEBE'>Code</Text>
+                    <VStack spacing='20px' bg={bgPrimary} padding='30px 30px' borderRadius='10px' width='100%'>
+                        <Box width='100%'>
+                            <Text color='white' width='100%' fontWeight='600' fontSize='30px'>GAME HUB</Text>
+                            <Text color='#BEBEBE' width='100%'>REACTJS + TYPESCRIPT + CHAKRA-UI</Text>
+                        </Box>
+                        <Text width='100%'>A video game discovery platform for game lovers.</Text>
+
+                        <HStack width='100%'>
+                            <HStack>
+                                <FontAwesomeIcon color="#BEBEBE" icon={faEye} />
+                                <Text color='#BEBEBE'>Live</Text>
+                            </HStack>
+
+                            <HStack>
+                                <FontAwesomeIcon color="#BEBEBE" icon={faCodeBranch} />
+                                <Text color='#BEBEBE'>Code</Text>
+                            </HStack>
                         </HStack>
-                    </HStack>
+                    </VStack>
                 </VStack>
+
+                <Text fontWeight='700' marginTop='50px'>OTHERS</Text>
+
+                <SimpleGrid columns={{ base: 2 }} spacing='10px'>
+                    <VStack spacing='20px' bg={bgPrimary} padding='30px 30px' borderRadius='10px'>
+                        <Box width='100%'>
+                            <Text color='white' width='100%' fontWeight='600' fontSize='30px'>GAME HUB</Text>
+                            <Text color='#BEBEBE' width='100%'>REACTJS + TYPESCRIPT + CHAKRA-UI</Text>
+                        </Box>
+                        <Text width='100%' margin='20px 0'>A video game discovery platform for game lovers.</Text>
+
+                        <HStack width='100%' spacing='30px'>
+                            <HStack>
+                                <FontAwesomeIcon color="#BEBEBE" icon={faEye} />
+                                <Text color='#BEBEBE'>Live</Text>
+                            </HStack>
+
+                            <HStack>
+                                <FontAwesomeIcon color="#BEBEBE" icon={faCodeBranch} />
+                                <Text color='#BEBEBE'>Code</Text>
+                            </HStack>
+                        </HStack>
+                    </VStack>
+
+                    <VStack spacing='20px' bg={bgPrimary} padding='30px 30px' borderRadius='10px'>
+                        <Box width='100%'>
+                            <Text color='white' width='100%' fontWeight='600' fontSize='30px'>GAME HUB</Text>
+                            <Text color='#BEBEBE' width='100%'>REACTJS + TYPESCRIPT + CHAKRA-UI</Text>
+                        </Box>
+                        <Text width='100%' margin='20px 0'>A video game discovery platform for game lovers.</Text>
+
+                        <HStack width='100%' spacing='30px'>
+                            <HStack>
+                                <FontAwesomeIcon color="#BEBEBE" icon={faEye} />
+                                <Text color='#BEBEBE'>Live</Text>
+                            </HStack>
+
+                            <HStack>
+                                <FontAwesomeIcon color="#BEBEBE" icon={faCodeBranch} />
+                                <Text color='#BEBEBE'>Code</Text>
+                            </HStack>
+                        </HStack>
+                    </VStack>
+
+                    <VStack spacing='20px' bg={bgPrimary} padding='30px 30px' borderRadius='10px'>
+                        <Box width='100%'>
+                            <Text color='white' width='100%' fontWeight='600' fontSize='30px'>GAME HUB</Text>
+                            <Text color='#BEBEBE' width='100%'>REACTJS + TYPESCRIPT + CHAKRA-UI</Text>
+                        </Box>
+                        <Text width='100%' margin='20px 0'>A video game discovery platform for game lovers.</Text>
+
+                        <HStack width='100%' spacing='30px'>
+                            <HStack>
+                                <FontAwesomeIcon color="#BEBEBE" icon={faEye} />
+                                <Text color='#BEBEBE'>Live</Text>
+                            </HStack>
+
+                            <HStack>
+                                <FontAwesomeIcon color="#BEBEBE" icon={faCodeBranch} />
+                                <Text color='#BEBEBE'>Code</Text>
+                            </HStack>
+                        </HStack>
+                    </VStack>
+
+                    <VStack spacing='20px' bg={bgPrimary} padding='30px 30px' borderRadius='10px'>
+                        <Box width='100%'>
+                            <Text color='white' width='100%' fontWeight='600' fontSize='30px'>GAME HUB</Text>
+                            <Text color='#BEBEBE' width='100%'>REACTJS + TYPESCRIPT + CHAKRA-UI</Text>
+                        </Box>
+                        <Text width='100%' margin='20px 0'>A video game discovery platform for game lovers.</Text>
+
+                        <HStack width='100%' spacing='30px'>
+                            <HStack>
+                                <FontAwesomeIcon color="#BEBEBE" icon={faEye} />
+                                <Text color='#BEBEBE'>Live</Text>
+                            </HStack>
+
+                            <HStack>
+                                <FontAwesomeIcon color="#BEBEBE" icon={faCodeBranch} />
+                                <Text color='#BEBEBE'>Code</Text>
+                            </HStack>
+                        </HStack>
+                    </VStack>
+
+                    <VStack spacing='20px' bg={bgPrimary} padding='30px 30px' borderRadius='10px'>
+                        <Box width='100%'>
+                            <Text color='white' width='100%' fontWeight='600' fontSize='30px'>GAME HUB</Text>
+                            <Text color='#BEBEBE' width='100%'>REACTJS + TYPESCRIPT + CHAKRA-UI</Text>
+                        </Box>
+                        <Text width='100%' margin='20px 0'>A video game discovery platform for game lovers.</Text>
+
+                        <HStack width='100%' spacing='30px'>
+                            <HStack>
+                                <FontAwesomeIcon color="#BEBEBE" icon={faEye} />
+                                <Text color='#BEBEBE'>Live</Text>
+                            </HStack>
+
+                            <HStack>
+                                <FontAwesomeIcon color="#BEBEBE" icon={faCodeBranch} />
+                                <Text color='#BEBEBE'>Code</Text>
+                            </HStack>
+                        </HStack>
+                    </VStack>
+
+                    <VStack spacing='20px' bg={bgPrimary} padding='30px 30px' borderRadius='10px'>
+                        <Box width='100%'>
+                            <Text color='white' width='100%' fontWeight='600' fontSize='30px'>GAME HUB</Text>
+                            <Text color='#BEBEBE' width='100%'>REACTJS + TYPESCRIPT + CHAKRA-UI</Text>
+                        </Box>
+                        <Text width='100%' margin='20px 0'>A video game discovery platform for game lovers.</Text>
+
+                        <HStack width='100%' spacing='30px'>
+                            <HStack>
+                                <FontAwesomeIcon color="#BEBEBE" icon={faEye} />
+                                <Text color='#BEBEBE'>Live</Text>
+                            </HStack>
+
+                            <HStack>
+                                <FontAwesomeIcon color="#BEBEBE" icon={faCodeBranch} />
+                                <Text color='#BEBEBE'>Code</Text>
+                            </HStack>
+                        </HStack>
+                    </VStack>
+                </SimpleGrid>
             </VStack>
-
-            <Text fontWeight='700' marginTop='50px'>OTHERS</Text>
-
-            <SimpleGrid columns={{ base: 2 }} spacing='10px'>
-                <VStack spacing='20px' bg={bgPrimary} padding='30px 30px' borderRadius='10px'>
-                    <Box width='100%'>
-                        <Text color='white' width='100%' fontWeight='600' fontSize='30px'>GAME HUB</Text>
-                        <Text color='#BEBEBE' width='100%'>REACTJS + TYPESCRIPT + CHAKRA-UI</Text>
-                    </Box>
-                    <Text width='100%' margin='20px 0'>A video game discovery platform for game lovers.</Text>
-
-                    <HStack width='100%' spacing='30px'>
-                        <HStack>
-                            <FontAwesomeIcon color="#BEBEBE" icon={faEye} />
-                            <Text color='#BEBEBE'>Live</Text>
-                        </HStack>
-
-                        <HStack>
-                            <FontAwesomeIcon color="#BEBEBE" icon={faCodeBranch} />
-                            <Text color='#BEBEBE'>Code</Text>
-                        </HStack>
-                    </HStack>
-                </VStack>
-
-                <VStack spacing='20px' bg={bgPrimary} padding='30px 30px' borderRadius='10px'>
-                    <Box width='100%'>
-                        <Text color='white' width='100%' fontWeight='600' fontSize='30px'>GAME HUB</Text>
-                        <Text color='#BEBEBE' width='100%'>REACTJS + TYPESCRIPT + CHAKRA-UI</Text>
-                    </Box>
-                    <Text width='100%' margin='20px 0'>A video game discovery platform for game lovers.</Text>
-
-                    <HStack width='100%' spacing='30px'>
-                        <HStack>
-                            <FontAwesomeIcon color="#BEBEBE" icon={faEye} />
-                            <Text color='#BEBEBE'>Live</Text>
-                        </HStack>
-
-                        <HStack>
-                            <FontAwesomeIcon color="#BEBEBE" icon={faCodeBranch} />
-                            <Text color='#BEBEBE'>Code</Text>
-                        </HStack>
-                    </HStack>
-                </VStack>
-
-                <VStack spacing='20px' bg={bgPrimary} padding='30px 30px' borderRadius='10px'>
-                    <Box width='100%'>
-                        <Text color='white' width='100%' fontWeight='600' fontSize='30px'>GAME HUB</Text>
-                        <Text color='#BEBEBE' width='100%'>REACTJS + TYPESCRIPT + CHAKRA-UI</Text>
-                    </Box>
-                    <Text width='100%' margin='20px 0'>A video game discovery platform for game lovers.</Text>
-
-                    <HStack width='100%' spacing='30px'>
-                        <HStack>
-                            <FontAwesomeIcon color="#BEBEBE" icon={faEye} />
-                            <Text color='#BEBEBE'>Live</Text>
-                        </HStack>
-
-                        <HStack>
-                            <FontAwesomeIcon color="#BEBEBE" icon={faCodeBranch} />
-                            <Text color='#BEBEBE'>Code</Text>
-                        </HStack>
-                    </HStack>
-                </VStack>
-
-                <VStack spacing='20px' bg={bgPrimary} padding='30px 30px' borderRadius='10px'>
-                    <Box width='100%'>
-                        <Text color='white' width='100%' fontWeight='600' fontSize='30px'>GAME HUB</Text>
-                        <Text color='#BEBEBE' width='100%'>REACTJS + TYPESCRIPT + CHAKRA-UI</Text>
-                    </Box>
-                    <Text width='100%' margin='20px 0'>A video game discovery platform for game lovers.</Text>
-
-                    <HStack width='100%' spacing='30px'>
-                        <HStack>
-                            <FontAwesomeIcon color="#BEBEBE" icon={faEye} />
-                            <Text color='#BEBEBE'>Live</Text>
-                        </HStack>
-
-                        <HStack>
-                            <FontAwesomeIcon color="#BEBEBE" icon={faCodeBranch} />
-                            <Text color='#BEBEBE'>Code</Text>
-                        </HStack>
-                    </HStack>
-                </VStack>
-
-                <VStack spacing='20px' bg={bgPrimary} padding='30px 30px' borderRadius='10px'>
-                    <Box width='100%'>
-                        <Text color='white' width='100%' fontWeight='600' fontSize='30px'>GAME HUB</Text>
-                        <Text color='#BEBEBE' width='100%'>REACTJS + TYPESCRIPT + CHAKRA-UI</Text>
-                    </Box>
-                    <Text width='100%' margin='20px 0'>A video game discovery platform for game lovers.</Text>
-
-                    <HStack width='100%' spacing='30px'>
-                        <HStack>
-                            <FontAwesomeIcon color="#BEBEBE" icon={faEye} />
-                            <Text color='#BEBEBE'>Live</Text>
-                        </HStack>
-
-                        <HStack>
-                            <FontAwesomeIcon color="#BEBEBE" icon={faCodeBranch} />
-                            <Text color='#BEBEBE'>Code</Text>
-                        </HStack>
-                    </HStack>
-                </VStack>
-
-                <VStack spacing='20px' bg={bgPrimary} padding='30px 30px' borderRadius='10px'>
-                    <Box width='100%'>
-                        <Text color='white' width='100%' fontWeight='600' fontSize='30px'>GAME HUB</Text>
-                        <Text color='#BEBEBE' width='100%'>REACTJS + TYPESCRIPT + CHAKRA-UI</Text>
-                    </Box>
-                    <Text width='100%' margin='20px 0'>A video game discovery platform for game lovers.</Text>
-
-                    <HStack width='100%' spacing='30px'>
-                        <HStack>
-                            <FontAwesomeIcon color="#BEBEBE" icon={faEye} />
-                            <Text color='#BEBEBE'>Live</Text>
-                        </HStack>
-
-                        <HStack>
-                            <FontAwesomeIcon color="#BEBEBE" icon={faCodeBranch} />
-                            <Text color='#BEBEBE'>Code</Text>
-                        </HStack>
-                    </HStack>
-                </VStack>
-            </SimpleGrid>
-        </VStack>
+        </Box>
     </>
 }
 
